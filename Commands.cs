0d1509b9a90f4aceabf747db1a6623eb5efadb4a
Bruno Szewczyk <81621691+srcguy@users.noreply.github.com>
2024-04-06 04:27:52 +0200

    b1.0 version

---
+++ Commands.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Cosmos.System.FileSystem;
+
+namespace CommandList
+{
+    public class Commands
+    {
+        public static void Help()
+        {
+            Console.WriteLine("\n  Navigation");
+            Console.WriteLine("  cd <directory>   - Change current directory");
+            Console.WriteLine("  ls               - List files and directories in the current directory");
+
+            Console.WriteLine("\n  File operations");
+            Console.WriteLine("  mkdir <directory>- Create a new directory");
+            Console.WriteLine("  rm <file>        - Delete a file");
+            Console.WriteLine("  rmdir <directory>- Delete a directory");
+            Console.WriteLine("  cat <file>       - Display a file");
+            Console.WriteLine("  touch <file>     - Create an empty file");
+            Console.WriteLine("  miv              - Open MIV");
+
+            Console.WriteLine("\n  System commands");
+            Console.WriteLine("  reboot           - Reboot the system");
+            Console.WriteLine("  help             - Show available commands");
+            Console.WriteLine("  cls              - Clear the console screen");
+            Console.WriteLine("  exit             - Shutdown the system\n");
+        }
+
+        public static string Calc(double a, double b)
+        {
+            return $"{a} + {b} = {a + b}\n{a} - {b} = {a - b}\n{a} / {b} = {a / b}\n{a} * {b} = {a * b}";
+        }
+
+        public static void Ls()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string[] directories = Directory.GetDirectories(currentDirectory);
+            string[] files = Directory.GetFiles(currentDirectory);
+
+            foreach (var directory in directories)
+            {
+                Console.WriteLine($"  DIR  : {Path.GetFileName(directory)}");
+            }
+
+            foreach (var file in files)
+            {
+                Console.WriteLine($"  FILE : {Path.GetFileName(file)}");
+            }
+        }
+
+        public static void Cat(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                string file = string.Join(" ", args.Skip(1));
+
+                try
+                {
+                    string content = File.ReadAllText(Directory.GetCurrentDirectory() + @"\"+ file);
+                    Console.WriteLine(content);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Usage: cat <file>");
+            }
+        }
+
+        public static void Touch(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                string file = string.Join(" ", args.Skip(1));
+
+                try
+                {
+                    File.Create(Directory.GetCurrentDirectory() + @"\" + file).Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Usage: touch <file>");
+            }
+        }
+
+        public static void Cd(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                string newDirectory = string.Join(" ", args.Skip(1));
+
+                if (newDirectory == "..")
+                {
+                    string currentDirectory = Directory.GetCurrentDirectory();
+                    string parentDirectory = Path.GetDirectoryName(currentDirectory);
+
+                    if (!string.IsNullOrEmpty(parentDirectory))
+                    {
+                        try
+                        {
+                            Directory.SetCurrentDirectory(parentDirectory);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error: {ex.Message}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: Already at the root directory.");
+                    }
+                }
+                else
+                {
+                    string targetDirectory = Path.Combine(Directory.GetCurrentDirectory(), newDirectory);
+
+                    try
+                    {
+                        if (Directory.Exists(targetDirectory))
+                        {
+                            Directory.SetCurrentDirectory(targetDirectory);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Error: Directory '{newDirectory}' not found.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Usage: cd <directory>");
+            }
+
+        }
+        public static void Mkdir(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                string directory = string.Join(" ", args.Skip(1));
+
+                try
+                {
+                    Directory.CreateDirectory(Directory.GetCurrentDirectory() + @"\" + directory);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error y: {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Usage: mkdir <directory>");
+            }
+        }
+        public static void Rm(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                string file = string.Join(" ", args.Skip(1));
+
+                try
+                {
+                    File.Delete(Directory.GetCurrentDirectory() + @"\" + file);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error : {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Usage: rm <file>");
+            }
+        }
+
+        public static void Rmdir(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                string directory = string.Join(" ", args.Skip(1));
+
+                try
+                {
+                    Directory.Delete(Directory.GetCurrentDirectory() + @"\" + directory);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error : {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Usage: rmdir <directory>");
+            }
+        }
+    }
+}
---
+++ Kernel.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+using MIVeditor;
+using CommandList;
+using System.IO;
+using Cosmos.System.FileSystem.VFS;
+using System.Reflection.PortableExecutable;
+
+
+namespace cosmos
+{
+    public class Kernel : Sys.Kernel
+    {
+        Sys.FileSystem.CosmosVFS fs = new Sys.FileSystem.CosmosVFS();
+
+        public static string file;
+
+        static void Eve()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            string currentDirectory = Directory.GetCurrentDirectory();
+
+            Console.Write($"{currentDirectory}>>");
+            string input = Console.ReadLine();
+            string[] args = input.Split(' ');
+            string command = args[0].ToLower();
+            switch (command)
+            {
+                case "calc":
+                    if (args.Length == 3 && double.TryParse(args[1], out double a) && double.TryParse(args[2], out double b))
+                    {
+                        Console.WriteLine(Commands.Calc(a, b));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid arguments");
+                    }
+                    break;
+                
+                //file manipulation
+                case "ls":
+                    Commands.Ls();
+                    break;
+                case "cat":
+                    Commands.Cat(args);
+                    break;
+                case "touch":
+                    Commands.Touch(args);
+                    break;
+                case "cd":
+                    Commands.Cd(args);
+                    break;
+                case "rm":
+                    Commands.Rm(args);
+                    break;
+                case "mkdir":
+                    Commands.Mkdir(args);
+                    break;
+                case "rmdir":
+                    Commands.Rmdir(args);
+                    break;
+                case "miv":
+                    MIV.StartMIV();
+                    break;
+
+                //system
+                case "exit":
+                    Sys.Power.Shutdown();
+                    break;
+                case "cls":
+                    Console.Clear();
+                    break;
+                case "reboot":
+                    Sys.Power.Reboot();
+                    break;
+                case "help":
+                    Commands.Help();
+                    break;
+
+                //other
+
+                case "date":
+                    Console.WriteLine(DateTime.Now);
+                    break;
+
+            }
+            Eve();
+        }
+
+        protected override void BeforeRun()
+        {
+            VFSManager.RegisterVFS(fs);
+
+            var available_space = fs.GetAvailableFreeSpace(@"0:\");
+            var fs_type = fs.GetFileSystemType(@"0:\");
+            Console.WriteLine("Available free space: " + available_space);
+            Console.WriteLine("File system: " + fs_type);
+            Console.WriteLine($"{Cosmos.Core.CPU.GetCPUBrandString()}");
+            Console.WriteLine($"RAM: {Cosmos.Core.CPU.GetAmountOfRAM()} MB");
+
+            Directory.SetCurrentDirectory(@"0:\");
+            string currentDirectory = Directory.GetCurrentDirectory();
+            Console.WriteLine("Current directory: " + currentDirectory);
+
+        }
+
+        protected override void Run()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("\nAdam b1.0 ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("by src_guy");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("booted succesfully \n");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("God's in his heaven. All's right with the world. \n");
+
+            Eve();
+        }
+    }
+}
